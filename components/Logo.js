9d8b1577bda7bcbf43fd135709186a9d56010aad
Ranjan <talk2ranjanktr@gmail.com>
2023-08-09 21:35:04 +0530

    Image tag change

--- components/Logo.js
+++ components/Logo.js
@@ -1,15 +1,27 @@
 import React from 'react'
+//import { Image } from 'expo-image';
+
 import { StyleSheet,View,Image } from 'react-native'
 const Logo = () => {
+    const blurhash =
+  '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
+
   return (
     <View style={styles.logoContainer}>
         <Image
           source={require('../assets/logo2.bmp')} 
         // source={{
-        //     uri: 'https://raw.githubusercontent.com/ranjankjha17/resturant-client-native/main/assets/logo2.bmp',
+        //     uri: 'https://ibb.co/DWdz1hW',
         //   }}
           style={styles.logo}
         />
+        {/* <Image
+        style={styles.logo}
+        source="https://picsum.photos/seed/696/3000/2000"
+        placeholder={blurhash}
+        contentFit="cover"
+        transition={1000}
+      /> */}
       </View>
   )
 }
